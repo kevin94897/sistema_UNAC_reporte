c95ac251eb907a95b46a6d547ca066dcbe735c42
KEVIN\user <kevin94897@gmail.com>
2019-02-18 16:44:43 -0500

    commit reportes

---
+++ application/libraries/pdf_report.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * 
+ */
+require_once dirname(__file__).'/tcpdf/tcpdf.php';
+
+class Pdf_report extends TCPDF
+{
+	protected $ci;
+
+	public function __construct()
+	{
+		$this->ci =& get_instance();
+	}
+}
